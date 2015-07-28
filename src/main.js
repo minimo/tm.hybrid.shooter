19312f68575c4e14d99cb9d650a0765c70029c0b
minimo <minimo.mobile@gmail.com>
2015-07-28 19:30:25 +0900

    change screen size

--- src/main.js
+++ src/main.js
@@ -4,14 +4,18 @@
  *  @auther minimo  
  *  This Program is MIT license.
  */
+
+var SC_W = 640;
+var SC_H = 640;
+
 tm.main(function() {
     var app = tm.hybrid.Application("#canvas2d", "#canvas3d");
-    app.fps = 30;
-    app.resize(640, 640).fitWindow().run();
+    app.fps = 60;
+    app.resize(SC_W, SC_H).fitWindow().run();
     app.enableStats();
     
     app.replaceScene(tm.game.LoadingScene({
-        width: 640, height: 960,
+        width: SC_W, height: SC_H,
         assets: {
         },
         nextScene: tmapp.gameScene,
